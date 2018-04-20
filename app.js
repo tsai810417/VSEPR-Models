7c2bb66184458c9f3106764d49a15f23f314b79c
tsai810417 <tsai_cheng@yahoo.com>
2018-04-20 09:49:51 -0700

    prepare for heroku

--- app.js
+++ app.js
@@ -15,7 +15,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('./'))
 app.get('/', (request, res) => {
